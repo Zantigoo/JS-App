5ece42ba32befaed71b2885fd7b57f63d0dfaf7f
Mathew Brookes <83518330+Zantigoo@users.noreply.github.com>
2021-07-24 02:51:18 -0400

    Updated Scripts.JS
    - Removed declaration of Burrito love.
    
    + Added three item array of pokemon objects.

--- script/scripts.js
+++ script/scripts.js
@@ -1,4 +1,5 @@
-alert('Hello world');
-
-let FavoriteFood = 'Burrito';
-document.write(FavoriteFood);
+let pokemonList = [
+    {name: "Drakloak", types:['Dragon','Ghost'], height: 5},
+    {name: "Shedinja", types:['Bug','Ghost'], height: 2},
+    {name: "Gourgeist", types:['Grass','Ghost'], height:3}
+];
