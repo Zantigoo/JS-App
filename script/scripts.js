7be8f054d6dadaa5a546c6939be4a0f38841de19
Mathew Brookes <83518330+Zantigoo@users.noreply.github.com>
2021-07-25 06:54:48 -0400

    Added functionality, styling & more
    + Page is now populated with entries from pokemonList array.
    + Added '⭐' to pokemon over 1 meter in height in recognition of their colossal status.
    + Added Gengar to pokemonList
    + Added Mimiku to pokemonList
    - Removed Drakloak from pokemonList
    
    + Added basic styling to page.
    ~ Page title changed from document to Pokedex

--- script/scripts.js
+++ script/scripts.js
@@ -1,5 +1,29 @@
+// Sets list of Pokemon as array
+// Array set as {name:"", types:['',''], height:#}
 let pokemonList = [
-    {name: "Drakloak", types:['Dragon','Ghost'], height: 5},
-    {name: "Shedinja", types:['Bug','Ghost'], height: 2},
-    {name: "Gourgeist", types:['Grass','Ghost'], height:3}
+    {name: "Mimikyu", types:[' Fairy',' Ghost'], height: 0.2},
+    {name: "Shedinja", types:[' Bug',' Ghost'], height: 0.8},
+    {name: "Gengar", types:[' Poison',' Ghost'], height:1.5},
+    {name: "Gourgeist", types:[' Grass',' Ghost'], height: 1.1},
 ];
+
+// Populates page with entries into the array with needed tags
+for (let i = 0; i < pokemonList.length; i++) {
+   document.write(
+    '<p class="entry">Name<span class="name">: '
+    + pokemonList[i].name
+    + ' </span> |'
+    + ' Types: <span class="types">' 
+    + pokemonList[i].types 
+    + '</span> | Height: <span class="height">' 
+    + pokemonList[i].height 
+    + '</span>m.' 
+    );
+    // Puts a little star next to pokemon over a metre
+    if (pokemonList[i].height > 1) {
+        document.write(' <span title="Big guy!">⭐</span></p>');
+    } else {
+        document.write('</p>');
+    }
+}
+
