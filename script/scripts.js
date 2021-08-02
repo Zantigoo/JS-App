87c3d34810c5ea90cbdc3aa904d67549afa696b7
Mathew Brookes <83518330+Zantigoo@users.noreply.github.com>
2021-08-02 03:33:24 -0400

    Update Scripts.js
    + Added pokeRepo to house list of pokemon
    + Added function to add new entries to pokemonList
    + Added function to pull list contents
    ~ Changed out for Loop with forEach function.
    ~ Changed function to populate so it can be used with different lists.

--- script/scripts.js
+++ script/scripts.js
@@ -1,29 +1,59 @@
 // Sets list of Pokemon as array
 // Array set as {name:"", types:['',''], height:#}
-let pokemonList = [
-    {name: "Mimikyu", types:[' Fairy',' Ghost'], height: 0.2},
-    {name: "Shedinja", types:[' Bug',' Ghost'], height: 0.8},
-    {name: "Gengar", types:[' Poison',' Ghost'], height:1.5},
-    {name: "Gourgeist", types:[' Grass',' Ghost'], height: 1.1},
-];
+
+let pokeRepo = (function() { //Protects dex list for future additions
+    let pokemonList = [ 
+        {name: "Mimikyu", types:['Fairy',' Ghost'], height: 0.2},
+        {name: "Shedinja", types:['Bug',' Ghost'], height: 0.8},
+        {name: "Gengar", types:['Poison',' Ghost'], height:1.5},
+        {name: "Gourgeist", types:['Grass',' Ghost'], height: 1.1},
+    ];
+
+//Add pokemon objects to array
+    function add(item) {
+        if (item.hasOwnProperty('name', 'types', 'height')) { //Sanitizes input (kinda)
+            return ( //Checks for object in function
+                typeof(item) == 'object' ? 
+                pokemonList.push(item) : 'Invalid entry'
+            );
+        } else {
+            console.log('Invalid Entry');
+        }
+    }
+//Fetches all the pokemon objects from the array
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+})();
 
 // Populates page with entries into the array with needed tags
-for (let i = 0; i < pokemonList.length; i++) {
+
+pokeRepo.getAll().forEach(dexDetails);
+
+function dexDetails(list) {
    document.write(
     '<p class="entry">Name<span class="name">: '
-    + pokemonList[i].name
+    + list.name
     + ' </span> |'
     + ' Types: <span class="types">' 
-    + pokemonList[i].types 
+    + list.types 
     + '</span> | Height: <span class="height">' 
-    + pokemonList[i].height 
+    + list.height 
     + '</span>m.' 
     );
     // Puts a little star next to pokemon over a metre
-    if (pokemonList[i].height > 1) {
+    if (list.height > 1) {
         document.write(' <span title="Big guy!">⭐</span></p>');
     } else {
         document.write('</p>');
     }
 }
+
+// Sanitizes add function
+
 
