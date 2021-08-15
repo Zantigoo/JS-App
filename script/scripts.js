d2bd586a2ad72b6660467a3381e59ab042ba51be
Mathew Brookes <83518330+Zantigoo@users.noreply.github.com>
2021-08-15 00:46:15 -0400

    Now contains all GEN 1 pokemon.
    Added sidebar with entries
    Laid ground work for detail cards
    Sidebar now opens and closes WOW!!

--- script/scripts.js
+++ script/scripts.js
@@ -2,14 +2,18 @@
 // Array set as {name:"", types:['',''], height:#}
 
 let pokeRepo = (function() { //Protects dex list for future additions
-    let pokemonList = [ 
+    let pokemonList = [];
+    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+
+//Gotta cut this during linting
+    let oldpokemonList = [ 
         {name: "Mimikyu", types:['Fairy',' Ghost'], height: 0.2},
         {name: "Shedinja", types:['Bug',' Ghost'], height: 0.8},
         {name: "Gengar", types:['Poison',' Ghost'], height:1.5},
         {name: "Gourgeist", types:['Grass',' Ghost'], height: 1.1},
     ];
 
-    //Add pokemon objects to array
+//Add pokemon objects to array
     function add(item) {
         if (item.hasOwnProperty('name', 'types', 'height')) { //Sanitizes input (kinda)
             return ( //Checks for object in function
@@ -21,7 +25,7 @@ let pokeRepo = (function() { //Protects dex list for future additions
         }
     }
 
-    //Fetches all the pokemon objects from the array
+//Fetches all the pokemon objects from the array
     function getAll() {
         return pokemonList;
     }
@@ -33,44 +37,100 @@ let pokeRepo = (function() { //Protects dex list for future additions
         let btn = document.createElement('button');
         btn.innerText = pokemon.name;
         btn.classList.add('bar-buttons');
-        btn.addEventListener('click', function() {
+        btn.addEventListener('click', () => {
             showDetails(pokemon);
         });
         listItem.appendChild(btn);
         sideList.appendChild(listItem);
     }
 
+    function loadList() {
+        return fetch(apiUrl).then(function (response) {
+            return response.json();
+        }).then(function (json) {
+            json.results.forEach(function (item) {
+                let pokemon = {
+                    name: item.name,
+                    detailsUrl: item.url,
+                };
+                add(pokemon);
+            });
+        }).catch(function(e) {
+            console.log(e);
+        })
+    }
+    
+    function loadDetails(item) {
+        let url = item.detailsUrl;
+        return fetch(url).then(function (response) {
+            return response.json();
+        }).then(function (details) {
+            item.imageUrl = details.spirtes.front_default;
+            item.height = details.height;
+            item.types = details.types;
+        }).catch(function(e) {
+            console.log(e);
+        })
+    }
+
     function showDetails(pokemon) {
-        console.log(pokemon);
+        loadDetails(pokemon).then(function () {
+            console.log(pokemon);
+        });
     }
 
     return {
         add: add,
         getAll: getAll,
+        loadList: loadList,
         addListItem: addListItem,
+        loadDetails:loadDetails
     };
 
 })();
 
-
-pokeRepo.getAll().forEach(function (pokemon) {
-    pokeRepo.addListItem(pokemon);
+pokeRepo.loadList().then(function() {
+    pokeRepo.getAll().forEach(function (pokemon) {
+        pokeRepo.addListItem(pokemon);
+    });
 });
 
+
 let navi = (function() {
-    function open() { //Open bar and push content
-        document.getElementById('pkmn-list').style.width = "150px";
-        document.getElementById('main').style.marginLeft = "150px";
+
+    
+    let state = "open"
+    
+    let open = () => { //Open bar and push content
+        document.getElementById('pkmn-list').style.width = "180px";
+        document.getElementById('main').style.marginLeft = "180px";
+        return state = "open";  
+        
     }
 
-    function close() { //Close bar and pull content
+    let close = () => { //Close bar and pull content
         document.getElementById('pkmn-list').style.width = "0";
         document.getElementById('main').style.marginLeft = "0";
+        return state = "close";
     }
 
+    let toggle = () => {
+        if (state === "close"){
+                open();
+                console.log('open')
+        } else {
+               close();
+               console.log('close')
+        }
+    }
+
+//Replace with CSS classes at some point
+
     return {
+        toggle: toggle,
         open: open,
         close: close,
     };
 
 })();
+
