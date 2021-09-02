426895879315863da014f12f2337e1e6eb731c63
Mathew Brookes <83518330+Zantigoo@users.noreply.github.com>
2021-09-02 17:40:24 -0400

    New pokemon modals
    + Added modals for pokedex entries making this whole app basically functional! WOO
    + minor interface improvements like larger scrollbar for the side menu

--- script/scripts.js
+++ script/scripts.js
@@ -44,6 +44,7 @@ let pokeRepo = (function() { //Protects dex list for future additions
         sideList.appendChild(listItem);
     }
 
+   
     function loadList() {
         return fetch(apiUrl).then(function (response) {
             return response.json();
@@ -51,7 +52,7 @@ let pokeRepo = (function() { //Protects dex list for future additions
             json.results.forEach(function (item) {
                 let pokemon = {
                     name: item.name,
-                    detailsUrl: item.url,
+                    detailsUrl: item.url
                 };
                 add(pokemon);
             });
@@ -60,34 +61,108 @@ let pokeRepo = (function() { //Protects dex list for future additions
         })
     }
     
+    
     function loadDetails(item) {
         let url = item.detailsUrl;
+        let name = item.name
         return fetch(url).then(function (response) {
             return response.json();
         }).then(function (details) {
-            item.imageUrl = details.spirtes.front_default;
+            item.name = name;
+            item.imageUrl = details.sprites.front_default;
             item.height = details.height;
+            item.weight = details.weight;
             item.types = details.types;
+            item.abilities = details.abilities;
+            item.artworkUrl = details.sprites.other.dream_world.front_default;
         }).catch(function(e) {
-            console.log(e);
+            console.error(e);
         })
     }
 
-    function showDetails(pokemon) {
-        loadDetails(pokemon).then(function () {
-            console.log(pokemon);
+    function showDetails(selectedPokemon) {
+        loadDetails(selectedPokemon).then(function () {
+            showModal(selectedPokemon);
         });
     }
+    
+    //Modal Stuff ===============================================
+    let modalContainer = document.querySelector('#modal-container');
+    function showModal(selectedPokemon) {
+
+        let pokemonTypes= modalContainer.querySelector(".type-list")
+        let pokemonName = modalContainer.querySelector(".pkmn-name")
+        let pokemonAbilities = modalContainer.querySelector(".ability-list")
+        let pokemonWeight = modalContainer.querySelector(".pkmn-weight")
+        let pokemonHeight = modalContainer.querySelector(".pkmn-height")
+        let pokemonArt = modalContainer.querySelector(".pkmn-art")
+
+
+        //Name & Artwork
+
+        pokemonName.innerText = selectedPokemon.name;
+
+        pokemonArt.setAttribute('src', selectedPokemon.artworkUrl);
+        pokemonArt.setAttribute('alt','Artwork of '+selectedPokemon.name+'.')
+
+        //Types & Abilities 
+
+        pokemonTypes.innerHTML = '';
+        selectedPokemon.types.forEach((type) => {
+          console.log(type.type.name);
+          let listItem = document.createElement("li");
+          listItem.innerText = type.type.name;
+          pokemonTypes.appendChild(listItem);
+        });
+
+        pokemonAbilities.innerHTML = '';
+        selectedPokemon.abilities.forEach((ability) => {
+            console.log(ability.ability.name);
+            let listItem = document.createElement("li");
+            listItem.innerText = ability.ability.name;
+            pokemonAbilities.appendChild(listItem);
+        });
+
+        //Height & Weight
+
+        pokemonWeight.innerText = selectedPokemon.weight / 10 + ' KG'
+        pokemonHeight.innerText = selectedPokemon.height / 10 + " M"
+
+        //Close Button
+        let closeButtonElement = document.createElement('button');
+        closeButtonElement.classList.add('modal-close');
+        closeButtonElement.innerText = 'Close';
+        closeButtonElement.addEventListener('click', closeModal);
+
+        // Presto, now you can see it.
+        modalContainer.classList.add('visible');
+    }
+    function closeModal() {
+        modalContainer.classList.remove('visible');
+    }
+    window.addEventListener('keydown', (e) => {
+        if (e.key === 'Escape' && modalContainer.classList.contains('visible')){
+            closeModal();
+        }
+    });
+    modalContainer.addEventListener('click', (e) => {
+        let target = e.target;
+        if (target === modalContainer) {
+            closeModal();
+        }
+    })
+    //end of modal stuff =======================================
 
     return {
         add: add,
         getAll: getAll,
         loadList: loadList,
         addListItem: addListItem,
-        loadDetails:loadDetails
+        loadDetails:loadDetails,
     };
 
 })();
+
 
 pokeRepo.loadList().then(function() {
     pokeRepo.getAll().forEach(function (pokemon) {
