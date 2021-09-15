084e76b4eceea5466f96fd6c9cdbba48f008f2f2
Mathew Brookes <83518330+Zantigoo@users.noreply.github.com>
2021-09-15 17:43:07 -0400

    Added type SVGs

--- script/scripts.js
+++ script/scripts.js
@@ -110,6 +110,7 @@ let pokeRepo = (function() { //Protects dex list for future additions
         let pokemonArt = modalContainer.querySelector(".pkmn-art")
         let pokemonNum = modalContainer.querySelector(".pkmn-num")
 
+
         //Name & Artwork & number
 
         pokemonName.innerText = selectedPokemon.name;
@@ -121,11 +122,108 @@ let pokeRepo = (function() { //Protects dex list for future additions
 
         pokemonTypes.innerHTML = '';
         selectedPokemon.types.forEach((type) => {
-          console.log(type.type.name);
-          let listItem = document.createElement("li");
-          listItem.classList.add('type')
-          listItem.innerText = type.type.name;
-          pokemonTypes.appendChild(listItem);
+            console.log(type.type.name);
+            let typesvg = document.createElement('img');
+            switch (type.type.name) {
+                case 'water':
+                    svg = 'img/water.svg'
+                    typesvg.classList.add('water');
+                    typesvg.classList.add('type-img');
+                    break;
+                case 'bug':
+                    svg = 'img/bug.svg'
+                    typesvg.classList.add('bug');
+                    typesvg.classList.add('type-img');
+                    break;
+                case 'dark':
+                    svg = 'img/dark.svg';
+                    typesvg.classList.add('dark');
+                    typesvg.classList.add('type-img');
+                    break;
+                case 'dragon':
+                    svg = 'img/dragon.svg';
+                    typesvg.classList.add('dragon');
+                    typesvg.classList.add('type-img');
+                    break;
+                case 'electric':
+                    svg = 'img/electric.svg';
+                    typesvg.classList.add('electric');
+                    typesvg.classList.add('type-img');
+                    break;
+                case 'fairy':
+                    svg = 'img/fairy.svg';
+                    typesvg.classList.add('fairy');
+                    typesvg.classList.add('type-img');
+                    break;
+                case 'fighting':
+                    svg = 'img/fighting.svg';
+                    typesvg.classList.add('fighting');
+                    typesvg.classList.add('type-img');
+                    break;
+                case 'fire':
+                    svg = 'img/fire.svg';
+                    typesvg.classList.add('fire');
+                    typesvg.classList.add('type-img');
+                    break;
+                case 'flying':
+                    svg = 'img/flying.svg';
+                    typesvg.classList.add('flying');
+                    typesvg.classList.add('type-img');
+                    break;
+                case 'ghost':
+                    svg = 'img/ghost.svg';
+                    typesvg.classList.add('ghost');
+                    typesvg.classList.add('type-img');
+                    break;
+                case 'grass':
+                    svg = 'img/grass.svg';
+                    typesvg.classList.add('grass');
+                    typesvg.classList.add('type-img');
+                    break;
+                case 'ground':
+                    svg = 'img/ground.svg';
+                    typesvg.classList.add('ground');
+                    typesvg.classList.add('type-img');
+                    break;
+                case 'ice':
+                    svg = 'img/ice.svg';
+                    typesvg.classList.add('ice');
+                    typesvg.classList.add('type-img');
+                    break;
+                case 'normal':
+                    svg = 'img/normal.svg';
+                    typesvg.classList.add('normal');
+                    typesvg.classList.add('type-img');
+                    break;
+                case 'poison':
+                    svg = 'img/poison.svg';
+                    typesvg.classList.add('poison');
+                    typesvg.classList.add('type-img');
+                    break;
+                case 'psychic':
+                    svg = 'img/psychic.svg';
+                    typesvg.classList.add('psychic');
+                    typesvg.classList.add('type-img');
+                    break;
+                case 'rock':
+                    svg = 'img/rock.svg';
+                    typesvg.classList.add('rock');
+                    typesvg.classList.add('type-img');
+                    break;
+                case 'steel':
+                    svg = 'img/steel.svg';
+                    typesvg.classList.add('steel');
+                    typesvg.classList.add('type-img');
+                    break;
+                default:
+                    type.type.name;
+                    break;
+            }
+            let listItem = document.createElement("li");
+            typesvg.setAttribute('src', svg );
+            typesvg.setAttribute('alt', type.type.name+' type pokemon');
+            pokemonTypes.appendChild(listItem);
+            listItem.appendChild(typesvg);
         });
 
         pokemonAbilities.innerHTML = '';
